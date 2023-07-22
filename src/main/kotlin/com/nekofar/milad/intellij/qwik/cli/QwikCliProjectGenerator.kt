6162ca02800621bbd7f56b1a484eaf40329c42da
Milad Nekofar <milad@nekofar.com>
2023-07-22 12:26:37 +0330

    refactor: update project generator for clarity
    This commit refactors the QwikCliProjectGenerator and QwikProjectTemplateFactory kotlin files to improve code readability and consistency. Specifically, the import statements have been reduced and simplified by importing the QwikBundle.message class directly, and redundant typing information has been removed. The generateInTemp method now returns true instead of false, reflecting a change in the project's temporary generation requirement. Also, code formatting in createTemplates method was adjusted for better readability.

--- src/main/kotlin/com/nekofar/milad/intellij/qwik/cli/QwikCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/qwik/cli/QwikCliProjectGenerator.kt
@@ -6,33 +6,31 @@ import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
-import com.nekofar.milad.intellij.qwik.QwikBundle
+import com.nekofar.milad.intellij.qwik.QwikBundle.message
 import com.nekofar.milad.intellij.qwik.QwikIcons
-import javax.swing.Icon
 
 class QwikCliProjectGenerator : NpmPackageProjectGenerator() {
     private val packageName = "create-qwik"
     private val npxCommand = "create-qwik"
 
-    override fun getName(): String = QwikBundle.message("qwik.project.generator.name")
+    override fun getName() = message("qwik.project.generator.name")
 
-    override fun getDescription(): String = QwikBundle.message("qwik.project.generator.description")
+    override fun getDescription() = message("qwik.project.generator.description")
 
-    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> = emptyArray()
+    override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
     override fun packageName(): String = packageName
 
-    override fun presentablePackageName(): String =
-        QwikBundle.message("qwik.project.generator.presentable.package.name")
+    override fun presentablePackageName() = message("qwik.project.generator.presentable.package.name")
 
     override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, npxCommand))
 
-    override fun generateInTemp(): Boolean = false
+    override fun generateInTemp() = true
 
     override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> =
         arrayOf("../" + project?.name.toString())
 
-    override fun getIcon(): Icon = QwikIcons.ProjectGenerator
+    override fun getIcon() = QwikIcons.ProjectGenerator
 }
